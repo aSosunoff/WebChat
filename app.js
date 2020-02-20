790efd17f471b920fe2e5d6514886ab6729ea2ef
asosunoff <al.sosunoff@gmail.com>
2020-02-20 23:25:22 +0300

    install bosy-parser, cookie-parser express-favicon, express-handlebars, hbs, morgan

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 const path = require('path');
-const errorhandler = require('errorhandler');
+const errorhandlerMiddleware = require('errorhandler');
+const faviconMiddleware = require('express-favicon');
+const morganMiddleware = require('morgan');
+const coocieParserMiddleware = require('cookie-parser');
+
+const expressHbs = require('express-handlebars');
+const hbs = require('hbs');
+const bodyParser = require ('body-parser');
 
 const config = require('./config/index');
 const logger = require('./libs/log')(module);
 
+// -----------------------------------------
+
 const app = express();
 
-const exception = (err, req, res, next) => {
-	if(app.get('env') == 'development'){
-		const errorHandler = errorhandler()
-		errorHandler(err, req, res, next);
-	} else {
-		res.sendStatus(500);
-	}
+const exphbs = expressHbs.create({
+    layoutsDir: "views/layouts", 
+    defaultLayout: "master",
+    extname: "hbs",
+    helpers: {
+        section: function(name, options) { 
+            if (!this._sections) 
+                this._sections = {};
+                
+            this._sections[name] = options.fn(this); 
+            return null;
+        },
+    }
+});
+
+hbs.registerPartials(path.join(__dirname, "views/partials"));
+
+const skip = (req, res)  => res.statusCode < 400;
+
+if(app.get('env') == 'development'){
+	app.use(morganMiddleware(':remote-addr :method :url :status :response-time ms - :res[content-length]', { skip }));
+} else {
+	app.use(morganMiddleware('tiny', { skip }));
 }
 
 const pageNotFound = (req, res, next) => {
@@ -22,9 +47,23 @@ const pageNotFound = (req, res, next) => {
 }
 
 app
+	.set("view engine", "hbs")
+	.engine('hbs', exphbs.engine)
+	.set("views", path.join(__dirname, 'views'))
+
+	.use(faviconMiddleware(__dirname + '/public/favicon.png'))
+	.use(bodyParser.json()) 									// req.body parse application/json
+	.use(bodyParser.urlencoded({ extended: false })) 			// req.body parse application/x-www-form-urlencoded 
+	.use(coocieParserMiddleware()) 								// req.cookies
+	.use(express.static(path.join(__dirname, 'public')))
+
 	.use((req, res, next) => {
-		if(req.url == '/')
-			res.end('Hello');
+		if(req.url == '/'){
+			res.render('index.hbs', {
+				title: 'Главная страница',
+				isHome: true
+			});
+		}
 		else
 			next();
 	})
@@ -34,14 +73,15 @@ app
 		else
 			next();
 	})
-	.use(exception)
+	.use((err, req, res, next) => {
+		if(app.get('env') == 'development'){
+			const errorHandler = errorhandlerMiddleware()
+			errorHandler(err, req, res, next);
+		} else {
+			res.sendStatus(500);
+		}
+	})
 	.use(pageNotFound)
 	.listen(config.get('port'), () => {
-		// logger.silly(`Сервер запущен на порту ${config.get('port')}`);
-		// logger.debug(`Сервер запущен на порту ${config.get('port')}`);
-		// logger.verbose(`Сервер запущен на порту ${config.get('port')}`);
-		// logger.http(`Сервер запущен на порту ${config.get('port')}`);
 		logger.info(`Сервер запущен на порту ${config.get('port')}`);
-		// logger.warn(`Сервер запущен на порту ${config.get('port')}`);
-		// logger.error(`Сервер запущен на порту ${config.get('port')}`);
 	});
