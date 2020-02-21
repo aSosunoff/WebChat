9ded6a65ad7ee6cc310488c375e7e2e785b1d47a
asosunoff <al.sosunoff@gmail.com>
2020-02-21 15:16:24 +0300

    add: create user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const crypto = require('crypto');
+const mongoose = require('../libs/mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String, 
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.methods.encryptPassword = function (password) {
+    return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+};
+
+userSchema.virtual('password')
+    .set(function(password){
+        this._plainPassword = password;
+        this.salt = Math.random() + '';
+        this.hashedPassword = this.encryptPassword(password);
+    })
+    .get(function(){
+        return this._plainPassword;
+    });
+
+userSchema.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword;
+}
+
+module.exports = mongoose.model('User', userSchema);
