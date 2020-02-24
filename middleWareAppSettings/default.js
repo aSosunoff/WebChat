90590e909e060957f23f152e30acaeae12814a92
asosunoff <al.sosunoff@gmail.com>
2020-02-24 15:26:13 +0300

    added express-session

--- middleWareAppSettings/default.js
+++ middleWareAppSettings/default.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
 const faviconMiddleware = require('express-favicon');
-const coocieParserMiddleware = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morganMiddleware = require('morgan');
+
+const coocieParserMiddleware = require('cookie-parser');
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo')(expressSession);
+
+const config = require('../config');
+const mongoose = require('../libs/mongoose');
 
 const skip = (req, res)  => res.statusCode < 400;
 
@@ -21,6 +27,20 @@ module.exports = (app, module) => {
         .use(bodyParser.json()) 									// req.body parse application/json
         .use(bodyParser.urlencoded({ extended: false })) 			// req.body parse application/x-www-form-urlencoded 
         .use(coocieParserMiddleware()) 								// req.cookies
+        .use(expressSession({
+            secret: config.get('session:secret'),
+            key: config.get('session:key'),
+            cookie: config.get('session:cookie'),
+            store: new connectMongo({
+                mongooseConnection: mongoose.connection
+            }),
+            resave: true,
+            saveUninitialized: true
+        }))
+        .use((req, res, next) => {
+            req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+            res.send("Visits:" + req.session.numberOfVisits);
+        })
         .use(express.static(path.join(rootDirName, 'public')))
         .use(require('../middleware/sendHttpError'));
 }
