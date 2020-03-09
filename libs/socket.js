9947f244a0aa645122a8486eb17242007f57a691
asosunoff <al.sosunoff@gmail.com>
2020-03-10 00:42:45 +0300

    rewrite to promice

--- libs/socket.js
+++ libs/socket.js
@@ -6,13 +6,62 @@ const HttpError = require('../error').HttpError;
 const logger = require("./log")(module);
 const User = require('../models/user');
 
+async function authSocket(socket, next) {
+    try {
+        const session = await new Promise((resolve, reject) => {
+            socket.handshake.cookie = cookie.parse(socket.handshake.headers.cookie);
+            let sidCookie = socket.handshake.cookie[config.get('session:key')];
+            let sid = coocieParserMiddleware.signedCookie(sidCookie, config.get('session:secret'));
+    
+            sessionStore.load(sid, (err, session) => {
+                if(err){
+                    reject(new HttpError(401, 'Сессий нет'));
+                } else {
+                    socket.handshake.session = session;
+                    resolve(session);
+                }                    
+            });
+        });
+        
+        await new Promise((resolve, reject) => {
+            if(!session.user){
+                logger.debug(`Анонимная сессия ${session.id}`);
+                reject(new HttpError(403, 'Анонимная сессия'));
+            }
+
+            logger.debug(`Подключен пользователь ${session.user}`);
+
+            User.findById(session.user, (err, user) => {
+                if(err) {
+                    return callback(err);
+                }
+
+                if(!user) {
+                    reject(new HttpError(403, 'Анонимная сессия'));
+                }
+
+                logger.debug(`Пользователь найден ${user}`);
+
+                socket.handshake.user = user;
+
+                resolve();
+            })
+        });
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
 module.exports = (server) => {
     const io = require("socket.io")(server, {
         origins: `localhost:${config.get("port")}`,
     });
 
     io.use((socket, next) => {
-        new Promise((resolve, reject) => {
+        authSocket(socket, next);
+        /* new Promise((resolve, reject) => {
             socket.handshake.cookie = cookie.parse(socket.handshake.headers.cookie);
             let sidCookie = socket.handshake.cookie[config.get('session:key')];
             let sid = coocieParserMiddleware.signedCookie(sidCookie, config.get('session:secret'));
@@ -53,7 +102,7 @@ module.exports = (server) => {
             });
         })
         .then(next)
-        .catch(next);
+        .catch(next); */
     });
     
     io.on("connection", function(socket) {
